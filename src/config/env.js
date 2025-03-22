1e8ccee01ab009c41f8879f62ae86b247b0ab519
Diegui3o <dmejiama@unsa.edu.pe>
2025-03-22 00:51:22 -0500

    arreglo

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { config } from 'dotenv';
 import env from 'env-var';
 
@@ -16,23 +15,3 @@ export const envs = {
     MOTORS_TOPIC: env.get('MOTORS_TOPIC').required().asString(),
     MODE_TOPIC: env.get('MODE_TOPIC').required().asString(),
 };
-=======
-require('dotenv').config();
-const { get } = require('env-var');
-
-const envs = {
-    PORT: get('PORT').required().asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
-    MQTT_BROKER_URL: get('MQTT_BROKER_URL').required().asString(),
-    CONTROL_TOPIC: get('CONTROL_TOPIC').required().asString(),
-    ANGLES_TOPIC: get('ANGLES_TOPIC').required().asString(),
-    RATES_TOPIC: get('RATES_TOPIC').required().asString(),
-    ACC_TOPIC: get('ACC_TOPIC').required().asString(),
-    GYRO_TOPIC: get('GYRO_TOPIC').required().asString(),
-    KALMAN_TOPIC: get('KALMAN_TOPIC').required().asString(),
-    MOTORS_TOPIC: get('MOTORS_TOPIC').required().asString(),
-    MODE_TOPIC: get('MODE_TOPIC').required().asString(),
-};
-
-module.exports = { envs };
->>>>>>> 5f824acfbbc60441a94b55e5f2cfa71d368d0975
